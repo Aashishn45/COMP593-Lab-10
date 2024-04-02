7fc77701c70150db8fa7ea6d1a9e0b84156e74b9
Aashish Neupane <156460188+Aashishn45@users.noreply.github.com>
2024-04-02 10:51:55 -0400

    updated poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -3,6 +3,8 @@ Library for interacting with the PokeAPI.
 https://pokeapi.co/
 '''
 import requests
+import image_lib
+import os
 
 POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
@@ -10,6 +12,7 @@ def main():
     # Test out the get_pokemon_into() function
     # Use breakpoints to view returned dictionary
     poke_info = get_pokemon_info("Rockruff")
+   
     return
 
 def get_pokemon_info(pokemon):
@@ -46,9 +49,48 @@ def get_pokemon_info(pokemon):
         print('failure')
         print(f'Response code: {resp_msg.status_code} ({resp_msg.reason})')
 
-    # TODO: Define function that gets a list of all Pokemon names from the PokeAPI
+    
+def get_pok_names():
+    print("Getting lists of pokemon names....", end="")
+    param = {
+        "limit": 2000,
+        "offset": 0,
+    }
+    rep_msg = requests.get(POKE_API_URL, params=param)
+    if rep_msg.status_code == requests.codes.ok:
+        print('success')
+        # Return dictionary of Pokemon info
+        pok_dic =  rep_msg.json()
+        return [pok["name"] for pok in pok_dic["results"]]
+    else:
+        print('failure')
+        print(f'Response code: {rep_msg.status_code} ({rep_msg.reason})')
+    return
 
-    # TODO: Define function that downloads and saves Pokemon artwork
+def get_pok_art(pokemon, image_dir):
+    pok_inf = get_pokemon_info(pokemon)
+    if not pok_inf:
+        return
+    
+    art_url = pok_inf["sprites"]["other"]["official-artwork"]["front_default"]
+    if not art_url:
+        print(f"No artwork found for {pokemon.capitalize()}.")
+        return
+    
+    file_ext = art_url.split(".")[-1]
+    img_path = os.path.join(image_dir, f"{pokemon}.{file_ext}")
+    if os.path.isfile(img_path):
+        print(f"{pokemon.capitalize()} artwork already exists.")
+
+    
+    img_data = image_lib.download_image(art_url)
+    if not img_data:
+        return
+    
+    
+    if image_lib.save_image_file(img_data, img_path):
+        return img_path
+    
 
 if __name__ == '__main__':
     main()
