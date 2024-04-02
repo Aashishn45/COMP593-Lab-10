1a265437416edf06cedafebed034c0f09b410a6c
Aashish Neupane <156460188+Aashishn45@users.noreply.github.com>
2024-04-02 11:04:55 -0400

    updated poke_image_viewer

--- poke_image_viewer.py
+++ poke_image_viewer.py
@@ -6,8 +6,8 @@ Description:
 Usage:
   python poke_image_viewer.py
 """
-from tkinter import *
-from tkinter import ttk
+import ctypes
+from tkinter import ttk, Tk, PhotoImage
 import os
 
 # Get the script and images directory
@@ -15,15 +15,27 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 images_dir = os.path.join(script_dir, 'images')
 
 # TODO: Create the images directory if it does not exist
+if not os.path.isdir(images_dir):
+    os.makedirs(images_dir)
+
 
 # Create the main window
 root = Tk()
 root.title("Pokemon Viewer")
 
 # TODO: Set the icon
+ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("PokeImageViewer.COMP593")
+root.iconbitmap(os.path.join(script_dir, "poke_ball.ico"))
 
 # TODO: Create frames
+frm = ttk.Frame(root)
+frm.grid(sticky="nsew")
+
 
 # TODO: Populate frames with widgets and define event handler functions
+image_pth = os.path.join(script_dir, "poke_ball.png")
+photo = PhotoImage(file=image_pth)
+img_label = ttk.Label(frm, image=photo)
+img_label.grid(row=0, column=0, padx=10, pady=10)
 
 root.mainloop()
